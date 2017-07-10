6d7170e240b72b15c957c0f7ec45eb0b11ea2cf4
Jaylinwang <jaylin0019@gmail.com>
2017-07-10 14:11:15 +0800

    进度条改变动画

--- examples/progress/index.vue
+++ examples/progress/index.vue
@@ -4,16 +4,19 @@
   <basic></basic>
   <h2>mini</h2>
   <mini></mini>
+  <h2>update</h2>
+  <update></update>
 </div>
 </template>
 
 <script>
 import basic from './basic.vue'
 import mini from './mini.vue'
+import update from './update.vue'
 
 export default {
   components: {
-    basic, mini
+    basic, mini, update
   }
 }
 </script>
---
+++ examples/progress/update.vue
@@ -0,0 +1,43 @@
+<template>
+<div style="width:500px;">
+<v-progress :percent="percent">
+</v-progress>
+<v-button-group>
+  <v-button @click="minus">
+    <v-icon
+      type="minus"></v-icon>
+  </v-button>
+  <v-button @click="plus">
+    <v-icon type="plus"></v-icon>
+  </v-button>
+</v-button-group>
+
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      percent: 0
+    }
+  },
+  methods: {
+    minus () {
+      if (this.percent > 0) {
+        this.percent -= 10
+      }
+    },
+    plus () {
+      if (this.percent < 100) {
+        this.percent += 10
+      }
+    }
+  }
+}
+</script>
+<style>
+.v-progress{
+  margin-bottom: 16px;
+}
+</style>
